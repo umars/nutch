ef50a225fc98cb4c189b3602940f17e057bdbae3
Umar Shah <umarshah@simplyphi.com>
2011-05-28 22:03:54 +0530

    ignored solr server exception on utf-8 error on char 0xffff , set number of reducers to 8

--- src/java/org/apache/nutch/indexer/IndexerMapReduce.java
+++ src/java/org/apache/nutch/indexer/IndexerMapReduce.java
@@ -35,161 +35,199 @@ import org.apache.nutch.scoring.ScoringFilterException;
 import org.apache.nutch.scoring.ScoringFilters;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
 import java.util.Collection;
 import java.util.Iterator;
 
 public class IndexerMapReduce extends Configured
-implements Mapper<Text, Writable, Text, NutchWritable>,
-          Reducer<Text, NutchWritable, Text, NutchDocument> {
+        implements Mapper<Text, Writable, Text, NutchWritable>,
+        Reducer<Text, NutchWritable, Text, NutchDocument> {
 
-  public static final Log LOG = LogFactory.getLog(IndexerMapReduce.class);
+    public static final Log LOG = LogFactory.getLog(IndexerMapReduce.class);
+    public static final String rregex = "[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+";
+    public static CharsetDecoder utf8Decoder = Charset.forName("UTF-8").newDecoder();
 
-  private IndexingFilters filters;
-  private ScoringFilters scfilters;
 
-  public void configure(JobConf job) {
-    setConf(job);
-    this.filters = new IndexingFilters(getConf());
-    this.scfilters = new ScoringFilters(getConf());
-  }
+    private IndexingFilters filters;
+    private ScoringFilters scfilters;
 
-  public void map(Text key, Writable value,
-      OutputCollector<Text, NutchWritable> output, Reporter reporter) throws IOException {
-    output.collect(key, new NutchWritable(value));
-  }
+    public void configure(JobConf job) {
+        setConf(job);
+        this.filters = new IndexingFilters(getConf());
+        this.scfilters = new ScoringFilters(getConf());
+        utf8Decoder.onMalformedInput(CodingErrorAction.IGNORE);
+        utf8Decoder.onUnmappableCharacter(CodingErrorAction.IGNORE);
+    }
 
-  public void reduce(Text key, Iterator<NutchWritable> values,
-                     OutputCollector<Text, NutchDocument> output, Reporter reporter)
-    throws IOException {
-    Inlinks inlinks = null;
-    CrawlDatum dbDatum = null;
-    CrawlDatum fetchDatum = null;
-    ParseData parseData = null;
-    ParseText parseText = null;
-    Content rawContent = null;
-    while (values.hasNext()) {
-      final Writable value = values.next().get(); // unwrap
-      if (value instanceof Inlinks) {
-        inlinks = (Inlinks)value;
-      } else if (value instanceof Content) {
-        rawContent = (Content)value;
-      } else if (value instanceof CrawlDatum) {
-        final CrawlDatum datum = (CrawlDatum)value;
-        if (CrawlDatum.hasDbStatus(datum))
-          dbDatum = datum;
-        else if (CrawlDatum.hasFetchStatus(datum)) {
-          // don't index unmodified (empty) pages
-          if (datum.getStatus() != CrawlDatum.STATUS_FETCH_NOTMODIFIED)
-            fetchDatum = datum;
-        } else if (CrawlDatum.STATUS_LINKED == datum.getStatus() ||
-                   CrawlDatum.STATUS_SIGNATURE == datum.getStatus() ||
-                   CrawlDatum.STATUS_PARSE_META == datum.getStatus()) {
-          continue;
-        } else {
-          throw new RuntimeException("Unexpected status: "+datum.getStatus());
+    public void map(Text key, Writable value,
+                    OutputCollector<Text, NutchWritable> output, Reporter reporter) throws IOException {
+        output.collect(key, new NutchWritable(value));
+    }
+
+    public void reduce(Text key, Iterator<NutchWritable> values,
+                       OutputCollector<Text, NutchDocument> output, Reporter reporter)
+            throws IOException {
+        Inlinks inlinks = null;
+        CrawlDatum dbDatum = null;
+        CrawlDatum fetchDatum = null;
+        ParseData parseData = null;
+        ParseText parseText = null;
+        Content rawContent = null;
+        while (values.hasNext()) {
+            final Writable value = values.next().get(); // unwrap
+            if (value instanceof Inlinks) {
+                inlinks = (Inlinks)value;
+            } else if (value instanceof Content) {
+                rawContent = (Content)value;
+            } else if (value instanceof CrawlDatum) {
+                final CrawlDatum datum = (CrawlDatum)value;
+                if (CrawlDatum.hasDbStatus(datum))
+                    dbDatum = datum;
+                else if (CrawlDatum.hasFetchStatus(datum)) {
+                    // don't index unmodified (empty) pages
+                    if (datum.getStatus() != CrawlDatum.STATUS_FETCH_NOTMODIFIED)
+                        fetchDatum = datum;
+                } else if (CrawlDatum.STATUS_LINKED == datum.getStatus() ||
+                        CrawlDatum.STATUS_SIGNATURE == datum.getStatus() ||
+                        CrawlDatum.STATUS_PARSE_META == datum.getStatus()) {
+                    continue;
+                } else {
+                    throw new RuntimeException("Unexpected status: "+datum.getStatus());
+                }
+            } else if (value instanceof ParseData) {
+                parseData = (ParseData)value;
+            } else if (value instanceof ParseText) {
+                parseText = (ParseText)value;
+            } else if (LOG.isWarnEnabled()) {
+                LOG.warn("Unrecognized type: "+value.getClass());
+            }
         }
-      } else if (value instanceof ParseData) {
-        parseData = (ParseData)value;
-      } else if (value instanceof ParseText) {
-        parseText = (ParseText)value;
-      } else if (LOG.isWarnEnabled()) {
-        LOG.warn("Unrecognized type: "+value.getClass());
-      }
+
+        if (fetchDatum == null || dbDatum == null
+                || parseText == null || parseData == null) {
+            return;                                     // only have inlinks
+        }
+
+        if (!parseData.getStatus().isSuccess() ||
+                fetchDatum.getStatus() != CrawlDatum.STATUS_FETCH_SUCCESS) {
+            return;
+        }
+
+        NutchDocument doc = new NutchDocument();
+        final Metadata metadata = parseData.getContentMeta();
+        Metadata mdt = parseData.getParseMeta();
+        String oce = "OriginalCharEncoding";
+        String cef = "CharEncodingForConversion";
+        String ce = "Content-Encoding";
+        System.out.println("-----------------\n" );
+        //System.out.println(oce + "  |  " + mdt.get(oce));
+        //System.out.println(cef + "  |  " + mdt.get(cef));
+        //System.out.println( ce + "  |  " + mdt.get(ce));
+//    String encoding = mdt.get(cef);
+//
+//    if (encoding == null){
+//        System.out.println(oce + "  |  " + mdt.get(oce));
+//        System.out.println( ce + "  |  " + mdt.get(ce));
+//        encoding = mdt.get(oce);
+//        if (encoding == null){
+//        encoding = "UTF-8";
+//        }
+//    }
+        /* string normalization to get rid of unicode decode errors
+        String htmlDoc = "";
+        String s1 = Normalizer.normalize(htmlDoc, Normalizer.Form.NFKD);
+
+        htmlDoc = new String(s1.replaceAll(regex, "").getBytes("ascii"), "ascii");
+        */
+        // add segment, used to map from merged index back to segment files
+        doc.add("segment", metadata.get(Nutch.SEGMENT_NAME_KEY));
+
+        // add digest, used by dedup
+        doc.add("digest", metadata.get(Nutch.SIGNATURE_KEY));
+
+        // add raw html content to doc
+        if (rawContent != null){
+            //String raw_html = Normalizer.normalize(new String(rawContent.getContent(), encoding), Normalizer.Form.NFKD);
+
+            ByteBuffer raw_data = ByteBuffer.wrap(rawContent.getContent());
+            //CharBuffer cr_html = utf8Decoder.decode(raw_data);
+            doc.add("raw_content" , raw_data);
+            doc.add("content_type", rawContent.getContentType());
+            LOG.warn("RAW Content ADDED" );
+        } else {
+            LOG.warn("No RAW Content " );
+        }
+
+        final Parse parse = new ParseImpl(parseText, parseData);
+        try {
+            // extract information from dbDatum and pass it to
+            // fetchDatum so that indexing filters can use it
+            final Text url = (Text) dbDatum.getMetaData().get(Nutch.WRITABLE_REPR_URL_KEY);
+            if (url != null) {
+                fetchDatum.getMetaData().put(Nutch.WRITABLE_REPR_URL_KEY, url);
+            }
+            // run indexing filters
+            doc = this.filters.filter(doc, parse, key, fetchDatum, inlinks);
+        } catch (final IndexingException e) {
+            if (LOG.isWarnEnabled()) { LOG.warn("Error indexing "+key+": "+e); }
+            return;
+        }
+
+        // skip documents discarded by indexing filters
+        if (doc == null) return;
+
+        float boost = 1.0f;
+        // run scoring filters
+        try {
+            boost = this.scfilters.indexerScore(key, doc, dbDatum,
+                    fetchDatum, parse, inlinks, boost);
+        } catch (final ScoringFilterException e) {
+            if (LOG.isWarnEnabled()) {
+                LOG.warn("Error calculating score " + key + ": " + e);
+            }
+            return;
+        }
+        // apply boost to all indexed fields.
+        doc.setWeight(boost);
+        // store boost for use by explain and dedup
+        doc.add("boost", Float.toString(boost));
+
+        output.collect(key, doc);
     }
 
-    if (fetchDatum == null || dbDatum == null
-        || parseText == null || parseData == null) {
-      return;                                     // only have inlinks
+    public void close() throws IOException { }
+
+    public static void initMRJob(Path crawlDb, Path linkDb,
+                                 Collection<Path> segments,
+                                 JobConf job) {
+
+        LOG.info("IndexerMapReduce: crawldb: " + crawlDb);
+        LOG.info("IndexerMapReduce: linkdb: " + linkDb);
+        LOG.info("IndexerMapReduce: geek4377 MOD mult red: ");
+
+        for (final Path segment : segments) {
+            LOG.info("IndexerMapReduces: adding segment: " + segment);
+            FileInputFormat.addInputPath(job, new Path(segment, CrawlDatum.FETCH_DIR_NAME));
+            FileInputFormat.addInputPath(job, new Path(segment, CrawlDatum.PARSE_DIR_NAME));
+            FileInputFormat.addInputPath(job, new Path(segment, ParseData.DIR_NAME));
+            FileInputFormat.addInputPath(job, new Path(segment, ParseText.DIR_NAME));
+            FileInputFormat.addInputPath(job, new Path(segment, Content.DIR_NAME));
+        }
+
+        FileInputFormat.addInputPath(job, new Path(crawlDb, CrawlDb.CURRENT_NAME));
+        FileInputFormat.addInputPath(job, new Path(linkDb, LinkDb.CURRENT_NAME));
+        job.setInputFormat(SequenceFileInputFormat.class);
+
+        job.setMapperClass(IndexerMapReduce.class);
+        job.setReducerClass(IndexerMapReduce.class);
+        job.setNumReduceTasks(8);
+
+        job.setOutputFormat(IndexerOutputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NutchWritable.class);
+        job.setOutputValueClass(NutchWritable.class);
     }
-
-    if (!parseData.getStatus().isSuccess() ||
-        fetchDatum.getStatus() != CrawlDatum.STATUS_FETCH_SUCCESS) {
-      return;
-    }
-
-    NutchDocument doc = new NutchDocument();
-    final Metadata metadata = parseData.getContentMeta();
-
-    // add segment, used to map from merged index back to segment files
-    doc.add("segment", metadata.get(Nutch.SEGMENT_NAME_KEY));
-
-    // add digest, used by dedup
-    doc.add("digest", metadata.get(Nutch.SIGNATURE_KEY));
-
-    // add raw html content to doc
-    if (rawContent != null){
-      doc.add("raw_content" , new String(rawContent.getContent()));
-      doc.add("content_type", rawContent.getContentType());
-        LOG.warn("RAW Content ADDED" );
-    } else {
-        LOG.warn("No RAW Content " );
-    }
-
-    final Parse parse = new ParseImpl(parseText, parseData);
-    try {
-      // extract information from dbDatum and pass it to
-      // fetchDatum so that indexing filters can use it
-      final Text url = (Text) dbDatum.getMetaData().get(Nutch.WRITABLE_REPR_URL_KEY);
-      if (url != null) {
-        fetchDatum.getMetaData().put(Nutch.WRITABLE_REPR_URL_KEY, url);
-      }
-      // run indexing filters
-      doc = this.filters.filter(doc, parse, key, fetchDatum, inlinks);
-    } catch (final IndexingException e) {
-      if (LOG.isWarnEnabled()) { LOG.warn("Error indexing "+key+": "+e); }
-      return;
-    }
-
-    // skip documents discarded by indexing filters
-    if (doc == null) return;
-
-    float boost = 1.0f;
-    // run scoring filters
-    try {
-      boost = this.scfilters.indexerScore(key, doc, dbDatum,
-              fetchDatum, parse, inlinks, boost);
-    } catch (final ScoringFilterException e) {
-      if (LOG.isWarnEnabled()) {
-        LOG.warn("Error calculating score " + key + ": " + e);
-      }
-      return;
-    }
-    // apply boost to all indexed fields.
-    doc.setWeight(boost);
-    // store boost for use by explain and dedup
-    doc.add("boost", Float.toString(boost));
-
-    output.collect(key, doc);
-  }
-
-  public void close() throws IOException { }
-
-  public static void initMRJob(Path crawlDb, Path linkDb,
-                           Collection<Path> segments,
-                           JobConf job) {
-
-    LOG.info("IndexerMapReduce: crawldb: " + crawlDb);
-    LOG.info("IndexerMapReduce: linkdb: " + linkDb);
-
-    for (final Path segment : segments) {
-      LOG.info("IndexerMapReduces: adding segment: " + segment);
-      FileInputFormat.addInputPath(job, new Path(segment, CrawlDatum.FETCH_DIR_NAME));
-      FileInputFormat.addInputPath(job, new Path(segment, CrawlDatum.PARSE_DIR_NAME));
-      FileInputFormat.addInputPath(job, new Path(segment, ParseData.DIR_NAME));
-      FileInputFormat.addInputPath(job, new Path(segment, ParseText.DIR_NAME));
-      FileInputFormat.addInputPath(job, new Path(segment, Content.DIR_NAME));
-    }
-
-    FileInputFormat.addInputPath(job, new Path(crawlDb, CrawlDb.CURRENT_NAME));
-    FileInputFormat.addInputPath(job, new Path(linkDb, LinkDb.CURRENT_NAME));
-    job.setInputFormat(SequenceFileInputFormat.class);
-
-    job.setMapperClass(IndexerMapReduce.class);
-    job.setReducerClass(IndexerMapReduce.class);
-
-    job.setOutputFormat(IndexerOutputFormat.class);
-    job.setOutputKeyClass(Text.class);
-    job.setMapOutputValueClass(NutchWritable.class);
-    job.setOutputValueClass(NutchWritable.class);
-  }
 }
--- src/java/org/apache/nutch/indexer/solr/SolrWriter.java
+++ src/java/org/apache/nutch/indexer/solr/SolrWriter.java
@@ -16,11 +16,8 @@
  */
 package org.apache.nutch.indexer.solr;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map.Entry;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.nutch.indexer.NutchDocument;
 import org.apache.nutch.indexer.NutchField;
@@ -28,63 +25,97 @@ import org.apache.nutch.indexer.NutchIndexWriter;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.common.SolrException;
 import org.apache.solr.common.SolrInputDocument;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class SolrWriter implements NutchIndexWriter {
 
-  private SolrServer solr;
-  private SolrMappingReader solrMapping;
+    private SolrServer solr;
+    private SolrMappingReader solrMapping;
+    CharsetDecoder utf8Decoder;
 
-  private final List<SolrInputDocument> inputDocs =
-    new ArrayList<SolrInputDocument>();
+    private final List<SolrInputDocument> inputDocs =
+            new ArrayList<SolrInputDocument>();
 
-  private int commitSize;
+    private int commitSize;
+    public static Log LOG = LogFactory.getLog( SolrWriter.class);
 
-  public void open(JobConf job, String name) throws IOException {
-    solr = new CommonsHttpSolrServer(job.get(SolrConstants.SERVER_URL));
-    commitSize = job.getInt(SolrConstants.COMMIT_SIZE, 1000);
-    solrMapping = SolrMappingReader.getInstance(job);
-  }
+    public void open(JobConf job, String name) throws IOException {
+        solr = new CommonsHttpSolrServer(job.get(SolrConstants.SERVER_URL));
+        commitSize = job.getInt(SolrConstants.COMMIT_SIZE, 100);
+        solrMapping = SolrMappingReader.getInstance(job);
+        utf8Decoder = Charset.forName("UTF-8").newDecoder();
+        utf8Decoder.onMalformedInput(CodingErrorAction.IGNORE);
+        utf8Decoder.onUnmappableCharacter(CodingErrorAction.IGNORE);
 
-  public void write(NutchDocument doc) throws IOException {
-    final SolrInputDocument inputDoc = new SolrInputDocument();
-    for(final Entry<String, NutchField> e : doc) {
-      for (final Object val : e.getValue().getValues()) {
-        inputDoc.addField(solrMapping.mapKey(e.getKey()), val, e.getValue().getWeight());
-        String sCopy = solrMapping.mapCopyKey(e.getKey());
-        if (sCopy != e.getKey()) {
-        	inputDoc.addField(sCopy, val, e.getValue().getWeight());	
+    }
+
+    public void write(NutchDocument doc) throws IOException {
+        final SolrInputDocument inputDoc = new SolrInputDocument();
+        for(final Entry<String, NutchField> e : doc) {
+            for (final Object val : e.getValue().getValues()) {
+                if (e.getKey().equalsIgnoreCase("raw_content") || e.getKey().equalsIgnoreCase("content")){
+                    ByteBuffer bbuf;
+                    if (val instanceof String){
+                        String cont = (String) val;
+                        bbuf = ByteBuffer.wrap(cont.getBytes("UTF-8"));
+                    } else {
+                        bbuf = (ByteBuffer) val;
+                    }
+                    CharBuffer cr_html = utf8Decoder.decode(bbuf);
+                    inputDoc.addField(solrMapping.mapKey(e.getKey()), cr_html.toString(), e.getValue().getWeight());
+                } else {
+                    inputDoc.addField(solrMapping.mapKey(e.getKey()), val, e.getValue().getWeight());
+                }
+
+                String sCopy = solrMapping.mapCopyKey(e.getKey());
+                if (sCopy != e.getKey()) {
+                    inputDoc.addField(sCopy, val, e.getValue().getWeight());
+                }
+            }
         }
-      }
+        inputDoc.setDocumentBoost(doc.getWeight());
+        inputDocs.add(inputDoc);
+        if (inputDocs.size() > commitSize) {
+            try {
+                solr.add(inputDocs);
+            } catch (SolrException sex){
+                LOG.error("SolrException", sex);
+            } catch (final SolrServerException e) {
+                LOG.error("SolrServerException", makeIOException(e));
+            }
+            inputDocs.clear();
+        }
     }
-    inputDoc.setDocumentBoost(doc.getWeight());
-    inputDocs.add(inputDoc);
-    if (inputDocs.size() > commitSize) {
-      try {
-        solr.add(inputDocs);
-      } catch (final SolrServerException e) {
-        throw makeIOException(e);
-      }
-      inputDocs.clear();
-    }
-  }
 
-  public void close() throws IOException {
-    try {
-      if (!inputDocs.isEmpty()) {
-        solr.add(inputDocs);
-        inputDocs.clear();
-      }
-      // solr.commit();
-    } catch (final SolrServerException e) {
-      throw makeIOException(e);
+    public void close() throws IOException {
+        try {
+            if (!inputDocs.isEmpty()) {
+                solr.add(inputDocs);
+                inputDocs.clear();
+            }
+            solr.commit();
+        }  catch (SolrException sex){
+            LOG.error("SolrException", sex);
+        } catch (final SolrServerException e) {
+            LOG.error("SolrServerException", makeIOException(e));
+        }
     }
-  }
 
-  public static IOException makeIOException(SolrServerException e) {
-    final IOException ioe = new IOException();
-    ioe.initCause(e);
-    return ioe;
-  }
+    public static IOException makeIOException(SolrServerException e) {
+        final IOException ioe = new IOException();
+        ioe.initCause(e);
+        return ioe;
+    }
 
 }
